4fc95e4b79988873dc7b8a6357e885ae3ae6f5b5
yedai13 <facundo.marin2012@gmail.com>
2021-12-10 10:14:19 -0300

    add dbcontext

---
+++ HotelMoxIt/Repositorio/Models/HotelDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositorio.Models
+{
+    public class HotelDBContext : DbContext
+    {
+        public HotelDBContext(DbContextOptions<HotelDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Habitacion> Habitacion { get; set; }
+        public DbSet<Reserva> Reserva { get; set; }
+
+    }
+}
